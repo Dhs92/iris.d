b857920a91a28087ffebe2525b1365db4758a980
Alex Forehand <forehand.alex@gmail.com>
2021-07-03 12:42:40 -0700

    Testing

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,15 @@ use pest_derive::*;
 struct CommandParser;
 
 fn main() {
-    println!("{:#?}", CommandParser::parse(Rule::commands, "SQUIT localhost :Test").unwrap())
+    println!("{:#?}", CommandParser::parse(Rule::commands, "PRIVMSG Yuuki,#foobar,GH0S1,yuuki@local.host :Fuk u!").unwrap());
+//     ('\u{01}'..='\u{09}').for_each(|c| print!("{}", c));
+//     println!(); 
+//     ('\u{0B}'..='\u{0C}').for_each(|c| print!("{}", c));
+//     println!();
+//     ('\u{0E}'..='\u{1F}').for_each(|c| print!("{}", c));
+//     println!();
+//     ('\u{21}'..='\u{3F}').for_each(|c| print!("{}", c));
+//     println!();
+//     ('\u{41}'..='\u{FF}').for_each(|c| print!("{}", c));
+//     println!();
 }
