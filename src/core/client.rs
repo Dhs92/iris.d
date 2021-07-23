7fb98322e74c9d355bab9d52a9942f1a731a9d81
Alex Forehand <forehand.alex@gmail.com>
2021-07-23 10:38:47 -0700

    Logging and internal API refactor

--- src/core/client.rs
+++ src/core/client.rs
@@ -1,10 +1,13 @@
-use std::{io::Error as IoError, net::IpAddr};
+use std::{fmt::{Display, Formatter, Result as FmtResult}, io::Result as IoResult, net::IpAddr};
 
 use bitflags::bitflags;
 use dns_lookup::lookup_addr;
 use tokio::net::TcpStream;
+use log::{error, debug};
+use serde::{Serialize, Deserialize};
 
 bitflags! {
+    #[derive(Serialize, Deserialize)]
     pub struct Modes: u8 {
         const INVISIBLE = 0b0000_0001;
         const NOTICES = 0b0000_0010;
@@ -13,66 +16,74 @@ bitflags! {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct LocalClient {
     nick: String,
+    user: String,
+    real_name: String,
     addr: IpAddr,
     host: String,
     mode: Modes,
-    connection: TcpStream,
+
+    // if connection is `None` user will be in invalid state
+    #[serde(skip)]
+    connection: Option<TcpStream>,
+}
+
+impl Display for LocalClient {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}!{}@{}", self.nick, self.user, self.host)
+    }
 }
 
 impl From<LocalClient> for User {
     fn from(client: LocalClient) -> Self {
         User {
-            nick: client.nick,
-            addr: client.addr,
-            host: client.host,
-            mode: client.mode,
+            client,
             registered_nicks: Vec::new(),
-            connection: client.connection,
         }
     }
 }
 
 impl LocalClient {
-    pub fn with(nick: &str, modes: u8, connection: TcpStream) -> Result<Self, IoError> {
+    pub fn with(nick: &str, user: &str, real_name: &str, modes: u8, connection: TcpStream) -> IoResult<Self> {
         let addr = connection.peer_addr()?.ip();
         let host = lookup_addr(&addr)?;
         let nick = nick.into();
+        let user = user.into();
+        let real_name = real_name.into();
+        let connection = Some(connection);
         let mode = match Modes::from_bits(modes) {
             Some(mode) => mode,
             None => {
                 // change to logging in the future
-                eprintln!("Invalid mode: {:#X}, using default: 0x0", modes);
+                error!("Invalid mode: {:#X}, using default: 0x0", modes);
                 Modes::empty()
             }
         };
 
-        Ok(Self {
+        let local_client = Self {
             nick,
+            user,
+            real_name,
             addr,
             host,
             mode,
             connection,
-        })
+        };
+
+        debug!("Creating LocalClient: {}", local_client);
+
+        Ok(local_client)
     }
 
     pub fn promote(self) -> User {
+        debug!("Promoting from LocalUser to User: {}", self);
         self.into()
     }
 }
 
 pub struct User {
-    nick: String,
-    addr: IpAddr,
-    host: String,
-    mode: Modes,
-    registered_nicks: Vec<String>,
-    connection: TcpStream,
-}
-
-pub struct LocalServer {
-    addr: IpAddr,
-    host: String,
+    client: LocalClient,
+    registered_nicks: Vec<String>
 }
